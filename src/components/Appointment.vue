72e8c690a7ea2d5e581e625b15374a38340a4667
Aek <thelaval.lol@gmail.com>
2017-08-24 15:46:03 +0700

    Add Appointment alert

--- src/components/Appointment.vue
+++ src/components/Appointment.vue
@@ -13,11 +13,29 @@
         <div class="col-xs-12">
           <div class="input-label">กรุณากรอกรหัสการนัดหมาย</div>
           <div class="col-xs-12 no-padding">
-            <input type="text" class="form-control input-lg" v-model="codeUrl">
+            <input type="text" class="form-control input-lg yellow" v-model="codeUrl">
             <span class="btn btn-warning btn-lg" @click="AcceptCode()">ยืนยัน</span>
           </div>
+          <div class="alert alert-danger col-xs-12" v-if="alert[0].name" :class="alert[0].class">{{alert[0].name}}</div>
           <div class="input-label col-xs-12 or-label"><span>หรือ</span></div>
-          <router-link :to="noCodeUrl" class="col-xs-12 no-padding btn btn-lg btn-success m-t-10">เข้าสู่สมุดวัคซีนโดยไม่ใช้รหัสการนัดหมาย</router-link>
+          <div>
+            <div class="input-label">กรุณากรอกรหัสสมาชิกของคนไข้</div>
+            <div class="col-xs-12 no-padding">
+              <input type="text" class="form-control input-lg blue" v-model="searchText">
+              <span class="btn btn-primary btn-lg" @click="SearchDogs()">ค้นหาคนไข้</span>
+            </div>
+          </div>
+          <div class="alert alert-danger col-xs-12" v-if="alert[1].name" :class="alert[1].class">{{alert[1].name}}</div>
+          <div v-if="dogList.length > 0" class="animated fadeIn">
+            <div class="input-label m-t-10 col-xs-12 no-padding">กรุณาเลือกสุนัขที่จะฉีดวัคซีน</div>
+            <div class="col-xs-12 no-padding">
+              <select class="form-control input-lg green" v-model="dog.model">
+                <option v-for="(dog, i) in dogList" :key="dog" :value="dog">{{dog}}</option>
+              </select> 
+              <span class="btn btn-default btn-lg" style="margin-top: -10px;">ดูรายละเอียด</span>
+              <span class="btn btn-success btn-lg" style="margin-top: -10px;" @click="MakeAppointment()">ยืนยัน</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -27,16 +45,33 @@
 <script>
 export default {
   methods: {
+    SearchDogs () {
+      this.alert[1].name = 'ไม่พบสมาชิกดังกล่าว'
+      this.alert[1].class = 'animated fadeIn'
+      this.dogList = ['A', 'B', 'C']
+      this.dog.model = this.dogList[0]
+    },
+    MakeAppointment () {
+      this.codeUrl = 0
+      this.AcceptCode()
+    },
     AcceptCode () {
       if (this.codeUrl) {
         this.$router.replace('/doctor/vaccination/' + this.codeUrl)
+      } else {
+        this.alert[0].name = 'ไม่พบการนัดหมายดังกล่าว'
+        this.alert[0].class = 'animated fadeIn'
       }
     }
   },
   data () {
     return {
+      alert: [{name: null, class: null}, {name: null, class: null}],
       codeUrl: null,
-      noCodeUrl: '/doctor/vaccination/0'
+      noCodeUrl: '/doctor/vaccination/0',
+      searchText: '',
+      dog: {model: ''},
+      dogList: []
     }
   }
 }
@@ -49,7 +84,7 @@ export default {
   .appointment {
     .white-card {
       margin-top: 3%;
-      padding: 0px 0px 30px 0px;
+      padding: 0px 0px 50px 0px;
       .card-content {
         padding: 80px 50px 50px 50px;
       }
@@ -77,31 +112,42 @@ export default {
         width: 75%;
         display: inline-block;
       }
+      .yellow {
+        border: 1px solid #e4ba24;
+      }
+      .blue {
+        border: 1px solid #69A5AD;
+      }
+      .green {
+        border: 1px solid #84bf09;
+        width: 55%;
+      }
       .form-control:focus,.form-control:active {
         border: 0px solid $brown-color;
         box-shadow: 0px 0px 10px rgb(	105, 165, 173);
       }
-      .manual {
-        width: 100%;
-      }
-      .btn-warning{
+      .btn{
         width: 20%;
         display: inline-block;
         margin-top: -5px;
         transition-duration: 0.5s;
       }
+      .btn-default {
+        border: 1px solid #84bf09;
+      }
       .alert {
+        margin-top: 5px;
         margin-bottom: 0px;
       }
       .or-label {
         font-weight: normal;
-        margin: 30px 0px 20px 0px;
+        margin: 30px 0px;
         text-align: center;
         span {
           font-size: 24px;
           padding: 5px 15px;
           color: white;
-          background-color: $brown-color;
+          background-color: $theme-color;
           border-radius: 50%;
         }
       }
