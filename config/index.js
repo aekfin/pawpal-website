c5382e51fc3495310e724fa7a8dad6aecb4edf53
Aek <thelaval.lol@gmail.com>
2017-09-11 21:27:09 +0700

    Change port to 10180

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 10120,
+    port: 10180,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
